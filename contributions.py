4a717106866709ecbfb314339802c8deb5f1f5d4
simonkrol <simonkrol11@gmail.com>
2017-06-12 18:50:43 -0400

    Add sms capability

--- contributions.py
+++ contributions.py
@@ -1,6 +1,10 @@
 import requests
+from twilio.rest import TwilioRestClient
+import environ
+import os
 from bs4 import BeautifulSoup
-url = 'https://github.com/simonkrol'
+username="simonkrol"
+url = 'https://github.com/'+username
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 #print(soup)
@@ -12,3 +16,15 @@ contributions=[]
 for link in soup.find_all('rect'):
     contributions.append(link.get('data-count'))
 print(contributions[-1])
+environ.set_env()
+
+client = TwilioRestClient(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
+
+if(contributions[-1]=='0'):
+	messageBody="You have yet to make a contribution on Github today. Please do so before midnight or you risk losing your streak."
+else:
+	messageBody="So far today, you have made "+contributions[-1]+" contributions on Github"
+client.messages.create(from_=os.environ['TWILIO_NUMBER'],
+					   to="(613) 402-1404",
+					   body=messageBody)
+
