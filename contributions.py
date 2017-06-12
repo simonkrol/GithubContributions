44dde5ef03ef0ae41543c6616576d10853252bad
simonkrol <simonkrol11@gmail.com>
2017-06-12 19:03:37 -0400

    Improve contribution scraper

--- contributions.py
+++ contributions.py
@@ -1,29 +1,26 @@
 import requests
 from twilio.rest import TwilioRestClient
-import environ
+import environ#File used to set environment variables
 import os
 from bs4 import BeautifulSoup
-username="simonkrol"
+username='cbkrol'
 url = 'https://github.com/'+username
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
-#print(soup)
-#tree = html.fromstring(response.content)
-#buyers = tree.xpath('//div[@title="buyer-name"]/text()')
-#contributions=tree.xpath('//*[@id="js-pjax-container"]/div/div[2]/div[3]/div[1]/div[2]/div[2]/div[1]/svg')
-#print(contributions)
-contributions=[]
-for link in soup.find_all('rect'):
-    contributions.append(link.get('data-count'))
-print(contributions[-1])
+
+# contributions=[]
+# for link in soup.find_all('rect'): #Gets a list of contribution numbers for each day 
+#     contributions.append(link.get('data-count')) 
+#print(contributions[-1])
+cont=(soup.find_all('rect')[-1]).get('data-count')
 environ.set_env()
 
 client = TwilioRestClient(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
 
-if(contributions[-1]=='0'):
-	messageBody="You have yet to make a contribution on Github today. Please do so before midnight or you risk losing your streak."
+if(cont=='0'):
+	messageBody=username+" has yet to make a contribution on Github today."
 else:
-	messageBody="So far today, you have made "+contributions[-1]+" contributions on Github"
+	messageBody="So far today,"+username+" has made "+cont+" contributions on Github"
 client.messages.create(from_=os.environ['TWILIO_NUMBER'],
 					   to=os.environ['MY_NUMBER'],
 					   body=messageBody)
