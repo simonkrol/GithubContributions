a1eb18a59c5614029bdf7398ea1b1c9143e6b53c
simonkrol <simonkrol11@gmail.com>
2017-06-14 12:49:57 -0400

    Differentiate between different commands and execute them

--- helper.py
+++ helper.py
@@ -4,7 +4,7 @@ import time
 
 
 
-def get_response(user):
+def get_contributions(user):
 	response = requests.get(("https://github.com/"+user))
 	soup = BeautifulSoup(response.text, 'lxml')
 	soup=soup.find_all('rect')
@@ -14,6 +14,17 @@ def get_response(user):
 	for link in range(len(soup)-1, -1,-1):
 		if(soup[link].get('data-date')==curr_day):
 			return get_message(soup[link].get('data-count'), user)
+
+
+def get_message(cont, user):
+	if(cont=='0'):
+		return(user+" has yet to make a contribution on Github today. ")
+	elif(cont=="1"):
+		return("So far today, "+user+" has made "+cont+" contribution on Github. ")
+	else:
+		return("So far today, "+user+" has made "+cont+" contributions on Github. ")
+
+
 def get_streak(user):
 	response = requests.get(("https://github.com/"+user))
 	soup = BeautifulSoup(response.text, 'lxml')
@@ -41,12 +52,16 @@ def get_streak(user):
 					return message
 
 
-def get_message(cont, user):
-	if(cont=='0'):
-		return(user+" has yet to make a contribution on Github today. ")
-	else:
-		return("So far today, "+user+" has made "+cont+" contributions on Github. ")
+def get_help():
+	response="Welcome to the github contributions viewer, start your text with the username of the github user you would like to inspect."
+	response+=" The second word in your text can be any of the following:"
+	response+=" 'contributions'(returns the number of contributions the user has made today), 'streak'(indicates the current contribution streak of the user)."
+	response+="\n\n Example: simonkrol streak"
+	return response
 
+def get_error(command):
+	response=command +" is not a valid command, please send the word 'about' for information on how to use the github contributions viewer."
+	return response
 
 def send_sms(message, number):
 	client = TwilioRestClient(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
--- server.py
+++ server.py
@@ -10,15 +10,19 @@ def sms():
     number = request.form['From']
     message_body = request.form['Body']
     body=message_body.split()
-    print(body)
-    new_message=helper.get_response(body[0])
-    streak_message=""
-    if(len(body)>1):
-    	if(body[1].lower()=="streak"):
-    		streak_message=helper.get_streak(body[0])
+    body.append("error")
+
+    message= {
+    	'about': "helper.get_help()",
+    	'contributions': "helper.get_contributions(body[0])",
+        'streak' : "helper.get_streak(body[0])"
+    }.get(body[-2].lower(), "helper.get_error(body[-2])")
+    message=eval(message)
+    if(body[0]=="help"):
+    	message=helper.get_help()
+
     resp = twiml.Response()
-    new_message=new_message+streak_message
-    resp.message(new_message)
+    resp.message(message)
     return str(resp)
 if __name__ == '__main__':
 	app.run()
