b6e9239b309466038c259c9d75cd86bb147fc7ad
simonkrol <simonkrol11@gmail.com>
2017-06-13 23:10:32 -0400

    Respond to text with commits for username

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 from flask import Flask, request
 from twilio import twiml
-import contributions
+import helper
  
 app = Flask(__name__)
  
  
-# @app.route('/sms', methods=['POST'])
-# def sms():
-# 	number = request.form['From']
-# 	message_body = request.form['Body']
-# 	cont=contributions.get_cont(message_body)
-# 	message=contributions.get_message(cont)
-# 	resp = twiml.Response()
-# 	resp.message(message)
-# 	return str(resp)
 @app.route('/sms', methods=['POST'])
 def sms():
     number = request.form['From']
     message_body = request.form['Body']
- 
+    new_message=helper.get_response(message_body)
     resp = twiml.Response()
-    resp.message('Hello {}, you said: {}'.format(number, message_body))
+    resp.message(new_message)
     return str(resp)
 if __name__ == '__main__':
 	app.run()
--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
 
 
